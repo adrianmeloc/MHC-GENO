dfb352ed9d8aa4d98613e49f2ae3979b5babcdda
adrianmeloc <100747208+adrianmeloc@users.noreply.github.com>
2022-11-22 01:44:33 -0500

    Update -stats.r

--- -stats.r
+++ -stats.r
@@ -35,21 +35,20 @@ for (i in 1:96)
   write.table(lista_seqfreq[[i]], (paste("seqdqa_exon2_",i,".txt",sep = "")), sep="\t", quote=FALSE, row.names = FALSE)
 }
 
-#line28:reads files each file based on the pattern of the name, this has to be adjusted according to...
-#		the names of the samples used.
-#line29:creates a list that contains a value of 1% of total number of sequences. This value can be
-#		adjusted. For example if we want a 5% threshold we can change the ".01" to ".05"
+#line28: reads files based on the name pattern, this has to be adjusted according to the names of the samples used.
+#line29:creates a list that contains a value of 1% of total number of sequences. This value can be adjusted. 
+#       For example if we want a 5% threshold we can change the ".01" to ".05"
 #line30:Creates a data frame, it is important to put first "as.data.frame" as these adds colums.
 #       The table function gives you a data frame with the frequency of apperances,
-#		here I indicate only to keep the second row of every 4 rows (that contains the sequence).
+#		    here I indicate only to keep the second row of every 4 rows (that contains the sequence).
 #line31: Creates a list in which only the sequences that a repetated more than 1% of the total sequences are kept
 #line32: Creates a list of the sequences without the forward primer and the reverse complement
-#		 of the reverse primer, this list will substitute the column, that contains the sequence,in the original file.
-#		 Forward primer= GTGCTGCAGGTGTAAACTTGTACCAG, Reverse complement of reverse primer=CAACTCTACCGCTGCTACCGGATCCGTG
-#		 In this line you can substitute for your own primers. Even if you are working with degenrate primers
-#		 You can substitute the degenerate base for [:alpha:]
+#		     of the reverse primer, this list will substitute the column, that contains the sequence,in the original file.
+#		     Forward primer= GTGCTGCAGGTGTAAACTTGTACCAG, Reverse complement of reverse primer=CAACTCTACCGCTGCTACCGGATCCGTG
+#		     In this line you can substitute for your own primers. Even if you are working with degenrate primers
+#		     You can substitute the degenerate base for [:alpha:]
 #line33: Substitutes the column of the sequence with the primers with a new column with sequences with no primers
 #line34: Creates a new column that will give the percent value for each sequence based on all the sequences
 #line35: Writes the file name. Keep in mind that for this step we are using the same iteration value (i)
-#		 that we are using to read the files so we can maintain consistency
+#		     that we are using to read the files so we can maintain consistency.
 #Now we use these newly created files in the MHC-GENO-final code.
